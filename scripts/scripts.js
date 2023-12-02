760136841890dd1846308b33f3c0d30b39b02767
Yui-0807 <131344066+Yui-0807@users.noreply.github.com>
2023-12-02 01:52:08 -0800

    create responsive web

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -19,5 +19,15 @@ function startup() {
       el.style.accentColor = event.target.value;
     });
   }
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
   
   
